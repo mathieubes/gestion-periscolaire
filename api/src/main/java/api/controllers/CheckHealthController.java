b7c618804c0a671ef4be83a320d134a4116339bb
Axpers <20193733@etud.univ-evry.fr>
2022-03-15 11:42:29 +0100

    Update : Check health route

--- api/src/main/java/api/controllers/CheckHealthController.java
+++ api/src/main/java/api/controllers/CheckHealthController.java
@@ -3,8 +3,9 @@ package api.controllers;
 import com.fasterxml.jackson.core.JsonProcessingException;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import api.services.JsonParserService;
@@ -15,7 +16,7 @@ public class CheckHealthController {
   @Autowired
   JsonParserService jsonParserService;
 
-  @GetMapping
+  @RequestMapping(value = "", method = RequestMethod.GET)
   public String checkHealth() throws JsonProcessingException {
     return jsonParserService.parseToJson("status", "Ok");
   }
