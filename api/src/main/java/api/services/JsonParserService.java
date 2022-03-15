df03ce4229325bead9060bdd35807991200fef88
Mathieu BES <mathieu.bes@renovationman.com>
2022-03-15 09:23:44 +0100

    Change throw exceptions

--- api/src/main/java/api/services/JsonParserService.java
+++ api/src/main/java/api/services/JsonParserService.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javax.annotation.PostConstruct;
 
 import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.springframework.context.annotation.Scope;
@@ -20,7 +21,7 @@ public class JsonParserService {
     this.objectMapper = new ObjectMapper();
   }
 
-  public String parseToJson(String key, Object message) throws JsonParseException, IOException {
+  public String parseToJson(String key, Object message) throws JsonProcessingException {
     return ("{" + this.objectMapper.writeValueAsString(key) + " : " + this.objectMapper.writeValueAsString(message)
         + "}");
   }
