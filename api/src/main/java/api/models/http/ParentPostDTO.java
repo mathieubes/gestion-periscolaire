cd35005475a6c7775012a7fe9d72afd3cc81c0e8
Axpers <20193733@etud.univ-evry.fr>
2022-03-22 13:31:52 +0100

    Fix : ParentPostDTO

--- api/src/main/java/api/models/http/ParentPostDTO.java
+++ api/src/main/java/api/models/http/ParentPostDTO.java
@@ -4,9 +4,9 @@ public class ParentPostDTO extends UserPostDTO {
 
     private float fiscalNumber;
 
-    public ParentPostDTO(String firstname, String lastname, String password, String email, String address, String phoneNumber, float fiscalNumber) {
+    public ParentPostDTO(String firstname, String lastname, String password, String email, String address,
+            String phoneNumber) {
         super(firstname, lastname, password, email, address, phoneNumber);
-        this.fiscalNumber = fiscalNumber;
     }
 
     public float getFiscalNumber() {
--- api/src/main/java/api/services/UserService.java
+++ api/src/main/java/api/services/UserService.java
@@ -23,23 +23,25 @@ public class UserService {
 
   @PostConstruct
   private void initFakeParents() {
-    addParent(new ParentPostDTO("Mathieu", "BES", "pwdTest1", "matDu91@gmail.fr", "surLaA6", "06qlqchose", 10000));
-    addParent(new ParentPostDTO("Garik", "DERMINJYAN", "pwdTest2", "gd@gmail.fr", "surLePeriph", "07qlqchose", 209));
-    addParent(new ParentPostDTO("Florian", "CARBONI", "pwdTest3", "bgDeL'IBGBI@gmail.fr", "versRis", "118 218", 22));
-    addParent(new ParentPostDTO("Fawaz", "MOUSSOUGAN", "pwdTest4", "criminal@gmail.fr", "unknown", "Call the police", 99999));
+    addParent(new ParentPostDTO("Mathieu", "BES", "pwdTest1", "matDu91@gmail.fr", "surLaA6", "06qlqchose"));
+    addParent(new ParentPostDTO("Garik", "DERMINJYAN", "pwdTest2", "gd@gmail.fr", "surLePeriph", "07qlqchose"));
+    addParent(new ParentPostDTO("Florian", "CARBONI", "pwdTest3", "bgDeL'IBGBI@gmail.fr", "versRis", "118 218"));
+    addParent(new ParentPostDTO("Fawaz", "MOUSSOUGAN", "pwdTest4", "criminal@gmail.fr", "unknown", "Call the police"));
   }
 
   public ArrayList<Parent> getParents() {
     return parents;
   }
 
-  public Parent addParent(ParentPostDTO parentDto) {
+  public Parent addParent(ParentPostDTO parentPostDto) {
     UUID id = UUID.randomUUID();
 
-    String saltedPassword = EnvGlobalUseService.getValue(EnvKey.SALT_HASH_KEY) + parentDto.getPassword();
+    String saltedPassword = EnvGlobalUseService.getValue(EnvKey.SALT_HASH_KEY) + parentPostDto.getPassword();
     String hashedPassword = Hashing.sha256().hashString(saltedPassword, StandardCharsets.UTF_8).toString();
 
-    Parent parent = new Parent(id, parentDto.getFirstname(), parentDto.getLastname(), hashedPassword, parentDto.getEmail(), parentDto.getAddress(), parentDto.getPhoneNumber(), parentDto.getFiscalNumber());
+    Parent parent = new Parent(id, parentPostDto.getFirstname(), parentPostDto.getLastname(), hashedPassword,
+        parentPostDto.getEmail(), parentPostDto.getAddress(), parentPostDto.getPhoneNumber(),
+        parentPostDto.getFiscalNumber());
     this.parents.add(parent);
     return parent;
   }
