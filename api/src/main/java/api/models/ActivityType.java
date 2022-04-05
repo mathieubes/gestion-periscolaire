2a2b5e5f410a2bba47ca8512bdd5952f19a2fe88
Mathieu BES <mathieu.bes@renovationman.com>
2022-04-05 11:21:10 +0200

    Quotient Familial

---
+++ api/src/main/java/api/models/ActivityType.java
@@ -0,0 +1,9 @@
+package api.models;
+
+public enum ActivityType {
+    CHILD_CARE,
+    SUPERVISED_STUDIES,
+    RECREATION_CENTER,
+    CAFETERIA,
+    SPORTS
+}
--- api/src/main/java/api/models/Parent.java
+++ api/src/main/java/api/models/Parent.java
@@ -1,18 +1,21 @@
 package api.models;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 public class Parent extends User {
   private float fiscalNumber;
   private ArrayList<Child> children;
+  private ArrayList<Child> dependentChildren;
 
   public Parent(UUID id, String firstname, String lastname, String password, String email, String address,
       String phoneNumber) {
     super(id, firstname, lastname, password, email, address, phoneNumber);
 
     this.fiscalNumber = 0;
-    this.children = new ArrayList<Child>();
+    this.children = new ArrayList();
+    this.dependentChildren = new ArrayList();
   }
 
   public float getFiscalNumber() {
@@ -22,4 +25,10 @@ public class Parent extends User {
   public ArrayList<Child> getChildren() {
     return children;
   }
+
+  public void addDependentChild(Child child) {
+    this.dependentChildren.add(child);
+  }
+
+  public int getDependentChildrenCount() { return dependentChildren.size(); }
 }
