6c86bf909a9dc7a5c04fdc64ac393be206d60858
Mathieu BES <mathieu.bes@renovationman.com>
2022-04-12 08:57:08 +0200

    -

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,22 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import { UsersPage } from './pages/UsersPage';
+import React, { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { AuthContext } from './contexts/auth-context'
+import { LoginPage } from './pages/login-page'
 
 export const App = () => {
-  return <Routes><Route path="users" element={<UsersPage />} /></Routes>;
-};
+  const [authUser, setAuthUser] = useState(null)
+
+  return (
+    <AuthContext.Provider value={{ authUser, setAuthUser }}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<AuthProtectedRoute />}>
+            <Route path="mathieu" />
+          </Route>
+
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthContext.Provider>
+  )
+}
