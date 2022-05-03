f845e7ae0d8b7b78fba6ecb4aad8ef05e5512b2e
GarikD <garikd@outlook.fr>
2022-05-03 11:51:51 +0200

    Add : ActivityService

---
+++ api/src/main/java/api/services/ActivityService.java
@@ -0,0 +1,36 @@
+package api.services;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.UUID;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+
+import api.models.activities.Activity;
+import api.models.http.ActivityPostDTO;
+
+@Service
+@Scope("singleton")
+public class ActivityService {
+  private ArrayList<Activity> activities = new ArrayList<>();
+
+  @PostConstruct
+  private void initFakeActivities() {
+    addActivity(new ActivityPostDTO("Test activity", new Date(), 60, 20, 1, 25.0));
+  }
+
+  public Activity addActivity(ActivityPostDTO activityPostDTO) {
+    UUID id = UUID.randomUUID();
+
+    Activity activity = new Activity(id, activityPostDTO);
+    this.activities.add(activity);
+    return activity;
+  }
+
+  public ArrayList<Activity> getActivities() {
+    return this.activities;
+  }
+}
