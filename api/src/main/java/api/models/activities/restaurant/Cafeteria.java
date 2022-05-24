95065dfe83e4829f1e04cb3f855b628f37c305fe
GarikD <garikd@outlook.fr>
2022-05-24 08:38:13 +0200

    Update : Cafeteria model

--- api/src/main/java/api/models/activities/restaurant/Cafeteria.java
+++ api/src/main/java/api/models/activities/restaurant/Cafeteria.java
@@ -1,19 +1,20 @@
 package api.models.activities.restaurant;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.UUID;
 
 import api.models.activities.Activity;
 
 public class Cafeteria extends Activity {
-  private Menu menu;
+  private ArrayList<Menu> menus;
 
   public Cafeteria(UUID id, String name, Date date, int duration, int capacity, int minSupervisors, double price) {
     super(id, name, date, duration, capacity, minSupervisors, price);
-    this.menu = new Menu();
+    this.menus = new ArrayList<Menu>();
   }
 
-  public Menu getMenu() {
-    return this.menu;
+  public ArrayList<Menu> getMenu() {
+    return this.menus;
   }
 }
